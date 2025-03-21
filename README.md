b89251f092ce701f8c261dec5ac0a7a5ba98a88e
sonja-ops <sodju@hotmail.com>
2025-03-21 20:07:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# obligatorisk-oppgave-4-3
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/sonja-ops/obligatorisk-oppgave-4-3)
---
+++ script.js
@@ -0,0 +1,9 @@
+const ? = document.querySelector("#convertButton");
+
+?.addEventListner("click", function () {
+  const unitInput = ?;
+  const unitfrom = document.querySelector("#unitFrom");
+  const unitTo = .querySelector("#?")
+
+  // Fylll inn og logg alle variabler for å se om vi henter riktig før vi går videre i koden
+});
